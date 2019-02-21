c24ad2c2740e38c342a230c2ce401d7496d5b30a
nkshigeru <1712694+nkshigeru@users.noreply.github.com>
2019-02-21 13:57:32 +0900

    Work on macOS (CMake + Unix Makefiles)

--- thirdparty/rtmidi.cmake
+++ thirdparty/rtmidi.cmake
@@ -2,12 +2,23 @@ add_library(rtmidi STATIC
   ${CMAKE_CURRENT_LIST_DIR}/rtmidi/RtMidi.h
   ${CMAKE_CURRENT_LIST_DIR}/rtmidi/RtMidi.cpp
 )
-target_compile_definitions(rtmidi
-  PUBLIC __WINDOWS_MM__
-)
 target_include_directories(rtmidi
   PUBLIC ${CMAKE_CURRENT_LIST_DIR}/rtmidi
 )
-target_link_libraries(rtmidi
-  Winmm.lib
-)
+if(MSVC)
+  target_compile_definitions(rtmidi
+    PUBLIC __WINDOWS_MM__
+  )
+  target_link_libraries(rtmidi
+    Winmm.lib
+  )
+endif()
+if(APPLE)
+  target_compile_definitions(rtmidi
+    PUBLIC __MACOSX_CORE__
+  )
+  target_link_libraries(rtmidi
+    "-framework CoreAudio"
+    "-framework CoreMIDI"
+  )
+endif()
